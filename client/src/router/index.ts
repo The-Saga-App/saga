bc2b0444c489cc3ecda50993c7beef4fc1c877dc
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2024-03-17 14:44:41 -0400

    Authentication with Auth0 and Supabase

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,19 +3,25 @@ import { authGuard,  } from "@auth0/auth0-vue";
 import HomeView from '../views/HomeView.vue'
 import CallbackView from '../views/CallbackView.vue'
 import { auth0 } from '../auth0';
+import { supabase } from '../db.ts';
 
 async function routeGuard(to: RouteLocation) {
   const result = await authGuard(to);
 
   if (result) {
     const token = await auth0.getAccessTokenSilently();
-    console.log(token);
+    console.log('auth0', token);
+    const res = await supabase.functions.invoke('create-session', { headers: { authorization: `Bearer ${token}` }});
+    const b = await supabase.auth.getUser(res.data.access_token);
+
+    console.log(await supabase.rpc('get_user_id_by_email', { email: 'hello@jonathandupre.com' }));
+
+    console.log(b);
     return true;
   }
 
   return false;
 }
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- supabase/functions/create-session/index.ts
+++ supabase/functions/create-session/index.ts
@@ -1,61 +1,90 @@
 /**
- * # create-session
- *
- *  After someone signed in using Auth0, we mint a new auth token so they can call Supabase
+ *  After user signed in using Auth0, we mint a new auth token so they can call Supabase
  */
 
-import { STATUS_CODE } from "https://deno.land/std@0.220.1/http/status.ts";
-import { jwtVerify, SignJWT, createRemoteJWKSet } from 'https://deno.land/x/jose@v5.2.3/index.ts';
-import { tryCall} from './try-call.ts';
+import { jwtVerify, SignJWT, createRemoteJWKSet, JWTPayload } from 'https://deno.land/x/jose@v5.2.3/index.ts';
+import { createClient } from 'npm:@supabase/supabase-js';
+import { corsHeaders } from '../_shared/cors.ts';
+
+const supabaseUrl = Deno.env.get("SUPABASE_URL") as string;
+const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") as string;
 
 const DB_JWT_SECRET = Deno.env.get("JWT_SECRET");
-const ISSUER = 'https://kernelquest-dev.us.auth0.com/';
-const AUDIENCE = 'http://localhost:3000';
+const AUTH0_URL = Deno.env.get("AUTH0_URL"); // https://TENANT.us.auth0.com
+const BACKEND_URL = Deno.env.get("BACKEND_URL"); // http://localhost:3000
 
 Deno.serve(async (req) => {
-  if (!DB_JWT_SECRET) { console.error('Missing jwt secret.'); return new Response("", { status: 500 }); }
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  if (!DB_JWT_SECRET) { 
+    console.error('Missing jwt secret.'); 
+    return new Response("", { status: 500, headers: { ...corsHeaders } });
+  }
 
   const token = req.headers.get('authorization')?.split(' ')[1]; // Bearer <token>
-  if (!token) { return new Response("Missing token", { status: 400 }); }
-
-
-  const [err, decoded] = await tryCall(decodeAuthToken, token);
-
-  console.log('Decoded:', decoded);
-  
-  if (err) {
-    console.log('Failed decoding the token', err);
-    return new Response("Token invalid or something", { status: STATUS_CODE.Forbidden });
+  if (!token) { 
+    return new Response("Missing token", { status: 400, headers: { ...corsHeaders } });
   }
-  
-  
-  const payload = {
-    userId: decoded.sub,
-    iat: decoded.iat,
-    exp: decoded.exp
-  };
-  
-const alg = 'HS256';
-const secret = new TextEncoder().encode(DB_JWT_SECRET);
 
-  return new Response(JSON.stringify({
-    access_key: await new SignJWT({ })
-      .setProtectedHeader({ alg })
-      .setSubject(decoded.sub)
-      .setIssuedAt(decoded.iat)
-      .setExpirationTime(decoded.exp)
-      .sign(secret)
-  }))
+  try {
+    const { sub, iat, exp } = await decodeAuthToken(token);
+
+    const { email } = await fetchUser(token);
+    
+    if (!sub || !iat || !exp) return new Response('', { status: 400} );
+
+    const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+
+    const { data: users } = await supabase.from('users').select('*').eq('email', email).limit(1);
+
+    let user = users ? users[0] : null;
+
+    if (!user) {
+      const { error, data } = await supabase.auth.admin.createUser({ email });
+      if (error) {
+        return new Response('', { status: 500 });
+      }
+      user = data.user;
+      console.log(data);
+    }
+
+    return new Response(JSON.stringify({
+      access_token: await new SignJWT({ })
+        .setProtectedHeader({ alg: 'HS256' })
+        .setSubject(user.id)
+        .setAudience('authenticated')
+        .setIssuedAt(new Date(iat * 1000))
+        .setExpirationTime(new Date(exp * 1000))
+        .sign(new TextEncoder().encode(DB_JWT_SECRET))
+    }), { status: 200, headers: { ...corsHeaders, 'Content-Type': 'application/json' } });
+  } catch (err) {
+    console.log(err);
+    return new Response("No", { status: 401, headers: { ...corsHeaders } });
+  }
 });
 
-async function decodeAuthToken(jwt: string) {
-  const JWKS = createRemoteJWKSet(new URL('https://kernelquest-dev.us.auth0.com/.well-known/jwks.json'));
-  const result = await jwtVerify(jwt, JWKS, {
-    issuer: ISSUER,
-    audience: AUDIENCE,
+async function decodeAuthToken(jwt: string): Promise<JWTPayload> {
+  const JWKS = createRemoteJWKSet(new URL(`${AUTH0_URL}/.well-known/jwks.json`));
+  const { payload } = await jwtVerify(jwt, JWKS, {
+    issuer: `${AUTH0_URL}/`,
+    audience: BACKEND_URL,
   });
 
-  console.log('result:', result);
+  return payload;
+}
 
-  return result;
+async function fetchUser (token: string) {
+  const response = await fetch(`${AUTH0_URL}/userinfo`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`
+    }
+  }).then((data) => data.json());
+
+  console.log(response);
+
+  return response;
 }
